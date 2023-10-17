35d1d4f5cda764acd7c7a0d133a47326332bdb7b
derfal <sohail.bounnite@gmail.com>
2023-10-17 01:39:32 +0100

    adding property rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,3 +14,24 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    # List of getter functions
+    @property
+    def width(self):
+        """Gets the value for width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Gets the value for height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """Gets the value for x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """Gets the value for y"""
+        return self.__y
