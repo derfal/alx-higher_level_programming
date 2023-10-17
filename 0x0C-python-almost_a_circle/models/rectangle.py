6aef1c3ee62c7a7d701b3348bb92922b839983e4
derfal <sohail.bounnite@gmail.com>
2023-10-17 01:45:27 +0100

    adding functionality and fixing errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -35,3 +35,53 @@ class Rectangle(Base):
     def y(self):
         """Gets the value for y"""
         return self.__y
+
+    # List of setter functions
+    @width.setter
+    def width(self, value):
+        """Sets the value for width"""
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Sets the value for height"""
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """Sets the value for x"""
+        if (type(value) is not int):
+            raise TypeError("x must be an integer")
+
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """Sets the value for y"""
+        if (type(value) is not int):
+            raise TypeError("y must be an integer")
+
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
+
+    def area(self):
+        """Defines the area of the rectangle"""
+        return (self.__height * self.__width)
+        
--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -27,38 +27,87 @@ class test_rectangle(unittest.TestCase):
         '''
         self.r = Rectangle(5, 10)
 
-    def tearDown(self):
-        '''
-            Deleting created instance
-        '''
-        del self.r
+        def tearDown(self):
+            '''
+                Deleting created instance
+            '''
+            del self.r
 
-    def test_width(self):
-        '''
-            Testing the Rectangle width getter
-        '''
-        self.assertEqual(5, self.r.width)
+        def test_width(self):
+            '''
+                Testing the Rectangle width getter
+            '''
+            self.assertEqual(5, self.r.width)
 
-    def test_height(self):
-        '''
-            Testing the Rectangle height getter
-        '''
-        self.assertEqual(10, self.r.height)
+        def test_height(self):
+            '''
+                Testing the Rectangle height getter
+            '''
+            self.assertEqual(10, self.r.height)
 
-    def test_x(self):
-        '''
-            Testing Rectangle x getter and setter
-        '''
+        def test_x(self):
+            '''
+                Testing Rectangle x getter and setter
+            '''
 
-        self.r.x = 54
-        self.assertEqual(54, self.r.x)
-        self.assertEqual(0, self.r.y)
+            self.r.x = 54
+            self.assertEqual(54, self.r.x)
+            self.assertEqual(0, self.r.y)
 
-    def test_y(self):
-        '''
-            Testing Rectangle y getter and setter
-        '''
+        def test_y(self):
+            '''
+                Testing Rectangle y getter and setter
+            '''
 
-        self.r.y = 45
-        self.assertEqual(45, self.r.y)
-        self.assertEqual(0, self.r.x)
+            self.r.y = 45
+            self.assertEqual(45, self.r.y)
+            self.assertEqual(0, self.r.x)
+
+    def test_arectangle_id(self):
+        '''
+            Test the id for Rectangle
+        '''
+        rect = Rectangle(1, 3, 0, 0, 199)
+        self.assertEqual(199, rect.id)
+
+    def test_width_string(self):
+        '''
+            Testing for other than int
+        '''
+        with self.assertRaises(TypeError):
+            rect = Rectangle("1", 5)
+
+    def test_width_bool(self):
+        '''
+            Testing for other than int
+        '''
+        with self.assertRaises(TypeError):
+            rect = Rectangle(True, 5)
+
+    def test_width_list(self):
+        '''
+            Testing for other than int
+        '''
+        with self.assertRaises(TypeError):
+            rect = Rectangle([10, 6], 5)
+
+    def test_height_string(self):
+        '''
+            Testing for other than int
+        '''
+        with self.assertRaises(TypeError):
+            rect = Rectangle(1, "5")
+
+    def test_height_bool(self):
+        '''
+            Testing for other than int
+        '''
+        with self.assertRaises(TypeError):
+            rect = Rectangle(1, True)
+
+    def test_height_list(self):
+        '''
+            Testing for other than int
+        '''
+        with self.assertRaises(TypeError):
+            rect = Rectangle(5, [10, 6])
